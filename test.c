b87c7e94d5e31e2abde547058b3f11f776842497
lyb <lyb@qq.com>
2019-03-05 22:33:08 +0800

    t

--- test.c
+++ test.c
@@ -4,17 +4,19 @@
 #define M 10
 #define N 10
 void main(){
-   int i,j,k;
-   char c;
+   int i,j;
+
    int arr[M][N];
    int *array;
    int (*p)[N];
    int *pp[M];
+
    array=&arr[0][0];
    //array=arr;
    for(i=0;i<M;i++)
       for(j=0;j<N;j++)
          arr[i][j]=10*i+j;
+
    for (i=0,j=1;i<M*N+4;i++,++j){
       printf("(%2d)",*(array++));
       if (j==N ){
@@ -35,8 +37,21 @@ void main(){
          printf("(%2d)",*(p+i)[j]);
        printf("\n");
    }
+    printf("the end\n");
+   for(i=0;i<M;i++){
+      for(j=0;j<N;j++)
+         printf("(%2d)",*p[i+j]);
+       printf("\n");
+   }
+   printf("the end\n");
+   for(i=0;i<M;i++){
+      for(j=0;j<N;j++)
+         printf("(%2d)",**(p+i+j));
+       printf("\n");
+   }
+    printf("the end\n");    
 
-
+ 
   // for(int k=0;k<500;k++)
    //	printf("The num is %d;char is %c.\n",k,k);
 }
