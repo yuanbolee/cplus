07fe8ada586eb11f67afbdc2cac701e919cc00f2
lyb <13992206684@163.com>
2019-03-06 00:07:47 +0800

    t

--- Array_Pointer.c
+++ Array_Pointer.c
@@ -1,39 +1,41 @@
 #include<stdio.h>
-#define M 10  //array row
-#define N 10  //array column
+#define ROW 10  //array row
+#define COL 10  //array column
 void main(){
    int i,j,k;
    char c;
-   int arr[M][N];
-   int *array;
-   int (*p)[N];
-   int *pp[M];
-   array=&arr[0][0];
+   int Array[ROW][COL];
+   int *array=&Array[0][0];
+   int (*p)[COL];
+   int *pp[ROW];
+   p=Array;
+
    //array=arr;
-   for(i=0;i<M;i++)
-      for(j=0;j<N;j++)
-         arr[i][j]=10*i+j;
-   for (i=0,j=1;i<M*N+4;i++,++j){
+   //init Array
+   for(i=0;i<ROW;i++)
+      for(j=0;j<COL;j++)
+         Array[i][j]=10*i+j;
+   
+   for (i=0,j=1;i<ROW*COL+4;i++,++j){
       printf("(%2d)",*(array++));
-      if (j==N ){
+      if (j==COL ){
          printf("\n");
          j=0;
       }
    }
    printf("the end\n");
-   p=arr;
-   for(i=0;i<M;i++){
-      for(j=0;j<N;j++)
+   for(i=0;i<ROW;i++){
+      for(j=0;j<COL;j++)
          printf("(%2d)",(*(p+i))[j]);
       printf("\n");
    }
    //also the same as p[i][j],*(p[i]+i),*(*(p+i)+j)
    printf("the end\n");
-   for(i=0;i<M;i++){
-      for(j=0;j<N;j++)
+   for(i=0;i<ROW;i++){
+      for(j=0;j<COL;j++)
          printf("(%2d)",*(p+i)[j]);
        printf("\n");
    }
-   *pp=&arr[0][0];
+   *pp=&Array[0][0];
 
 }
