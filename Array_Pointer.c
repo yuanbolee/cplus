39fa4e07cd3b261a9672c4be071da4a057c26092
lyb <13992206684@163.com>
2019-03-07 15:55:13 +0800

    uu

--- Array_Pointer.c
+++ Array_Pointer.c
@@ -43,7 +43,11 @@ void main(){
    p=&Array[0][0];
    printf("The size of (char=%ld),(int=%ld)\n",sizeof(char),sizeof(int));
    printf("The address of (Array=%ld),(p=%ld),(pa=%ld),(ap=%ld)\n",\
-      (unsigned long)Array,(unsigned long)p,(unsigned long)pa,(unsigned long)ap);
+      (unsigned long)Array,(unsigned long)p,\
+      (unsigned long)pa,(unsigned long)ap);
+   printf("The address add 1 of (Array+1=%ld),(p+1=%ld),(pa+1=%ld),(ap+1=%ld)\n",\
+      (unsigned long)(Array+1),(unsigned long)(p+1),\
+      (unsigned long)(pa+1),(unsigned long)(ap+1));
    printf("The size of (Array[][]=%ld),(&Array=%ld),(Array=%ld)\n",\
       sizeof(Array[0][0]),sizeof(&Array),sizeof(Array));
    printf("The size of (p=%ld),(pa=%ld),(ap=%ld)\n",\
