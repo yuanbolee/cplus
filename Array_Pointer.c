e01372f4ed7c6b5b80eaa00ae13577428055c991
lyb <13992206684@163.com>
2019-03-07 14:09:18 +0800

    gg

--- Array_Pointer.c
+++ Array_Pointer.c
@@ -2,12 +2,12 @@
 #define ROW 10  //array row
 #define COL 10  //array column
 void main(){
-   int i,j,k;
-   int Array[ROW][COL];
-   int *ap=&Array[0][0];
-   int (*p)[COL];  //int pointer
-   int *pp[ROW];
-   p=Array;
+   int i,j;
+   int Array[ROW][COL];//array
+   int *p=&Array[0][0];// pointer of int
+   int (*pa)[COL];  //int pointer of array
+   int *ap[ROW];  //array of pointer
+   pa=Array;
 
    //array=arr;
    //init Array
@@ -16,7 +16,7 @@ void main(){
          Array[i][j]=10*i+j;
    
    for (i=0,j=1;i<ROW*COL+4;i++,++j){
-      printf("(%2d)",*(ap++));
+      printf("(%2d)",*(p++));
       if (j==COL ){
          printf("\n");
          j=0;
@@ -25,16 +25,16 @@ void main(){
    printf("the end\n");
    for(i=0;i<ROW;i++){
       for(j=0;j<COL;j++)
-         printf("(%2d)",(*(p+i))[j]);
+         printf("(%2d)",(*(pa+i))[j]);
       printf("\n");
    }
    //also the same as p[i][j],*(p[i]+i),*(*(p+i)+j)
    printf("the end\n");
    for(i=0;i<ROW;i++){
       for(j=0;j<COL;j++)
-         printf("(%2d)",*(p+i)[j]);
+         printf("(%2d)",*(pa+i)[j]);
        printf("\n");
    }
-   *pp=&Array[0][0];
+   *ap=&Array[0][0];
 
 }
