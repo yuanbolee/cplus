9b3c14b2bdfeaaec1934b86b541837d1d6f9aa09
lyb <13992206684@163.com>
2019-03-07 17:30:01 +0800

    55

--- Array_Pointer.c
+++ Array_Pointer.c
@@ -27,32 +27,40 @@ void main(){
    for(i=0;i<ROW;i++){
       for(j=0;j<COL;j++)
          printf("(%2d)",(*(pa+i))[j]);
-         //also the same as p[i][j],*(p[i]+i),*(*(p+i)+j)
+         //also the same as pa[i][j],*(pa[i]+j),*(*(pa+i)+j)
       printf("\n");
    }
    printf("the end\n");
-   printf("Output Array by use pa as ap.\n");
+   printf("Output Array by use pa as ap that is error.\n");
    for(i=0;i<ROW;i++){
       for(j=0;j<COL;j++)
-         printf("(%2d)",*(pa+i)[j]);
+         printf("(%2d)",*(pa+i)[j]);//pa use as ap method
        printf("\n");
    }
-
+   printf("the end\n");
+   printf("Output Array by use pa.\n");
+   for(i=0;i<ROW;i++){
+      for(j=0;j<COL;j++)
+      printf("(%2d)", (int *)(*(pa+i)[0]+j));
+      printf("\n");
+   }
+   printf("the end\n");
    //*ap=&Array[0][0];
    pa=Array;
    p=&Array[0][0];
-   printf("The size of (char=%ld),(int=%ld)\n",sizeof(char),sizeof(int));
-   printf("The address of (Array=%ld),(p=%ld),(pa=%ld),(ap=%ld)\n",\
-      (unsigned long)Array,(unsigned long)p,\
-      (unsigned long)pa,(unsigned long)ap);
-   printf("The address add 1 of (Array+1=%ld),(p+1=%ld),(pa+1=%ld),(ap+1=%ld)\n",\
-      (unsigned long)(Array+1),(unsigned long)(p+1),\
-      (unsigned long)(pa+1),(unsigned long)(ap+1));
+   printf("The size of (char=%ld),(int=%ld),(long=%ld)\n",\
+      sizeof(char),sizeof(int),sizeof(long));
+   printf("The (address +1) subtract address  equal (Array=%ld),(p=%ld),(pa=%ld),(ap=%ld)\n",\
+      (unsigned long)(Array+1)-(unsigned long)Array,\
+      (unsigned long)(p+1)-(unsigned long)p,\
+      (unsigned long)(pa+1)-(unsigned long)pa,\
+      (unsigned long)(ap+1)-(unsigned long)ap);
    printf("The size of (Array[][]=%ld),(&Array=%ld),(Array=%ld)\n",\
       sizeof(Array[0][0]),sizeof(&Array),sizeof(Array));
    printf("The size of (p=%ld),(pa=%ld),(ap=%ld)\n",\
       sizeof(p),sizeof(pa),sizeof(ap));
    printf("The size of (*p=%ld),(*pa=%ld),(*ap=%ld)\n",\
       sizeof(*p),sizeof(*pa),sizeof(*ap));
+   printf("NOTICE:\nThe results are different of 32bit or 64bit compiler");
 
 }
