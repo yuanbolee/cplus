5da196a0d000ef0a8acb98c4a4f8e873062dfadb
“LYB” <13992206684@163.com>
2019-03-13 15:54:28 +0800

    FSFSD

---
+++ Month_Week.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+enum Week{
+    Monday=1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+enum Month{
+    January=1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+int main(){
+    enum Week w;
+    enum Month m;
+    return 0;
+}
